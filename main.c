5572297de9b4ba6b20d37c5a9eb35940de95e448
jonathandw743 <jonathandw414@gmail.com>
2024-03-02 15:15:25 +0000

    all moves and pseudo random shuffle

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+#define SOLVED_CUBE                                                            \
+  {                                                                            \
+    .cornerPieces = {                                                          \
+      {.a = White, .b = Orange, .c = Blue},                                    \
+      {.a = White, .b = Blue, .c = Red},                                       \
+      {.a = White, .b = Red, .c = Green},                                      \
+      {.a = White, .b = Green, .c = Orange},                                   \
+      {.a = Yellow, .b = Orange, .c = Green},                                  \
+      {.a = Yellow, .b = Green, .c = Red},                                     \
+      {.a = Yellow, .b = Red, .c = Blue},                                      \
+      {.a = Yellow, .b = Blue, .c = Orange},                                   \
+    }                                                                          \
+  }
+
 #define STICKER "  "
 
 #define ANSI_BG_RED "\x1b[41m"
@@ -201,29 +215,182 @@ void move_d_prime(struct Cube *cube) {
   cube->cornerPieces[7] = tempPiece;
 }
 
+struct CornerPiece rotate_corner_piece_cw(struct CornerPiece cornerPiece) {
+  enum Colour tempColour = cornerPiece.c;
+  cornerPiece.c = cornerPiece.b;
+  cornerPiece.b = cornerPiece.a;
+  cornerPiece.a = tempColour;
+  return cornerPiece;
+}
+
+struct CornerPiece rotate_corner_piece_acw(struct CornerPiece cornerPiece) {
+  enum Colour tempColour = cornerPiece.a;
+  cornerPiece.a = cornerPiece.b;
+  cornerPiece.b = cornerPiece.c;
+  cornerPiece.c = tempColour;
+  return cornerPiece;
+}
+
+void move_f(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[4];
+  cube->cornerPieces[4] = rotate_corner_piece_cw(cube->cornerPieces[5]);
+  cube->cornerPieces[5] = rotate_corner_piece_acw(cube->cornerPieces[2]);
+  cube->cornerPieces[2] = rotate_corner_piece_cw(cube->cornerPieces[3]);
+  cube->cornerPieces[3] = rotate_corner_piece_acw(tempPiece);
+}
+
+void move_f_prime(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[3];
+  cube->cornerPieces[3] = rotate_corner_piece_acw(cube->cornerPieces[2]);
+  cube->cornerPieces[2] = rotate_corner_piece_cw(cube->cornerPieces[5]);
+  cube->cornerPieces[5] = rotate_corner_piece_acw(cube->cornerPieces[4]);
+  cube->cornerPieces[4] = rotate_corner_piece_cw(tempPiece);
+}
+
+void move_b(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[6];
+  cube->cornerPieces[6] = rotate_corner_piece_cw(cube->cornerPieces[7]);
+  cube->cornerPieces[7] = rotate_corner_piece_acw(cube->cornerPieces[0]);
+  cube->cornerPieces[0] = rotate_corner_piece_cw(cube->cornerPieces[1]);
+  cube->cornerPieces[1] = rotate_corner_piece_acw(tempPiece);
+}
+
+void move_b_prime(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[1];
+  cube->cornerPieces[1] = rotate_corner_piece_acw(cube->cornerPieces[0]);
+  cube->cornerPieces[0] = rotate_corner_piece_cw(cube->cornerPieces[7]);
+  cube->cornerPieces[7] = rotate_corner_piece_acw(cube->cornerPieces[6]);
+  cube->cornerPieces[6] = rotate_corner_piece_cw(tempPiece);
+}
+
+void move_r(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[5];
+  cube->cornerPieces[5] = rotate_corner_piece_cw(cube->cornerPieces[6]);
+  cube->cornerPieces[6] = rotate_corner_piece_acw(cube->cornerPieces[1]);
+  cube->cornerPieces[1] = rotate_corner_piece_cw(cube->cornerPieces[2]);
+  cube->cornerPieces[2] = rotate_corner_piece_acw(tempPiece);
+}
+
+void move_r_prime(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[2];
+  cube->cornerPieces[2] = rotate_corner_piece_acw(cube->cornerPieces[1]);
+  cube->cornerPieces[1] = rotate_corner_piece_cw(cube->cornerPieces[6]);
+  cube->cornerPieces[6] = rotate_corner_piece_acw(cube->cornerPieces[5]);
+  cube->cornerPieces[5] = rotate_corner_piece_cw(tempPiece);
+}
+
+void move_l(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[7];
+  cube->cornerPieces[7] = rotate_corner_piece_cw(cube->cornerPieces[4]);
+  cube->cornerPieces[4] = rotate_corner_piece_acw(cube->cornerPieces[3]);
+  cube->cornerPieces[3] = rotate_corner_piece_cw(cube->cornerPieces[0]);
+  cube->cornerPieces[0] = rotate_corner_piece_acw(tempPiece);
+}
+
+void move_l_prime(struct Cube *cube) {
+  struct CornerPiece tempPiece = cube->cornerPieces[0];
+  cube->cornerPieces[0] = rotate_corner_piece_acw(cube->cornerPieces[3]);
+  cube->cornerPieces[3] = rotate_corner_piece_cw(cube->cornerPieces[4]);
+  cube->cornerPieces[4] = rotate_corner_piece_acw(cube->cornerPieces[7]);
+  cube->cornerPieces[7] = rotate_corner_piece_cw(tempPiece);
+}
+
+void no_op_move(struct Cube *cube) { printf("no-op move\n"); }
+
+// doesn't need to be the most perfect hash
+unsigned int move_hash(int seed, int x) {
+  return ((unsigned int)((((x * 78234567) % 23453) + seed) * 23483) % 12);
+}
+
+void (*move(unsigned int move_id))(struct Cube *cube) {
+  switch (move_id) {
+  case 0:
+    return move_u;
+  case 1:
+    return move_u_prime;
+  case 2:
+    return move_d;
+  case 3:
+    return move_d_prime;
+  case 4:
+    return move_f;
+  case 5:
+    return move_f_prime;
+  case 6:
+    return move_b;
+  case 7:
+    return move_b_prime;
+  case 8:
+    return move_r;
+  case 9:
+    return move_r_prime;
+  case 10:
+    return move_l;
+  case 11:
+    return move_l_prime;
+  default:
+    return no_op_move;
+  }
+}
+
+int move_code(char *buffer, int write_start_pos, unsigned int move_id) {
+  switch (move_id) {
+  case 0:
+    return copy_string(buffer, write_start_pos, "U");
+  case 1:
+    return copy_string(buffer, write_start_pos, "U'");
+  case 2:
+    return copy_string(buffer, write_start_pos, "D");
+  case 3:
+    return copy_string(buffer, write_start_pos, "D'");
+  case 4:
+    return copy_string(buffer, write_start_pos, "F");
+  case 5:
+    return copy_string(buffer, write_start_pos, "F'");
+  case 6:
+    return copy_string(buffer, write_start_pos, "B");
+  case 7:
+    return copy_string(buffer, write_start_pos, "B'");
+  case 8:
+    return copy_string(buffer, write_start_pos, "R");
+  case 9:
+    return copy_string(buffer, write_start_pos, "R'");
+  case 10:
+    return copy_string(buffer, write_start_pos, "L");
+  case 11:
+    return copy_string(buffer, write_start_pos, "L'");
+  default:
+    return copy_string(buffer, write_start_pos, "?");
+  }
+}
+
+void shuffle(int seed, struct Cube *cube) {
+  char shuffle_code[151];
+  int end = 0;
+  for (int i = 0; i < 50; i++) {
+    unsigned int move_id = move_hash(seed, i);
+    move(move_id)(cube);
+    end = move_code(shuffle_code, end, move_id);
+    end = copy_string(shuffle_code, end, " ");
+  }
+  printf("%s\n", shuffle_code);
+}
+
 int main() {
-  struct Cube cube = {.cornerPieces = {
-                          {.a = White, .b = Orange, .c = Blue},
-                          {.a = White, .b = Blue, .c = Red},
-                          {.a = White, .b = Red, .c = Green},
-                          {.a = White, .b = Green, .c = Orange},
-                          {.a = Yellow, .b = Orange, .c = Green},
-                          {.a = Yellow, .b = Green, .c = Red},
-                          {.a = Yellow, .b = Red, .c = Blue},
-                          {.a = Yellow, .b = Blue, .c = Orange},
-                      }};
+  struct Cube cube = SOLVED_CUBE;
 
   char buffer[1024];
 
   cube_net_string(buffer, &cube);
-  printf("%s", buffer);
+  printf("%s\n", buffer);
 
-  for (int i = 0; i < 7; i++) {
-    move_d_prime(&cube);
-  }
+  int seed;
+  scanf("%d", &seed);
+
+  shuffle(seed, &cube);
 
   cube_net_string(buffer, &cube);
-  printf("%s", buffer);
+  printf("%s\n", buffer);
 
   return 0;
 }
